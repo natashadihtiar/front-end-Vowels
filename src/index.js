57dded4071e3224f59722181c339e773c2ac7064
natashadihtiar <47831860+natashadihtiar@users.noreply.github.com>
2020-02-27 15:18:53 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 function getAllVowels(string) {
     const vowels = ["A", "a", "E", "e", "O", "o", "И", "и", "І", "і"];
-    let newString = "";
+    let count = 0;
     for (let i = 0; i < string.length; i++) {
-        if(vowels.includes(string[i])) {
-            newString += string[i];
+        for (let j = 0; j < vowels.length; j++) {
+            if(string[i] === vowels[j]) {
+            count++;
+            }
         }
     }
-return newString.length;
+return count;
 }
-
